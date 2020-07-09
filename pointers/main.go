59b1b6a59826dd79396e0955d081d210fb5d01a4
deveshbajpai <devesh_bajpai@apple.com>
2020-07-09 19:34:14 +0530

    pointers with structs

--- pointers/main.go
+++ pointers/main.go
@@ -1,1 +1,38 @@
-package pointers
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main()  {
+
+	alex := person{
+		firstName: "Alex",
+		lastName: "Anderson",
+	}
+
+	alex.updateFirstName1("Alexis")
+	fmt.Println(alex) //update is not reflected, as the struct is passed as value to the method
+
+	(&alex).updateFirstName2("Alexis")
+	fmt.Println(alex) //update is reflected, as the struct is passed as reference to the method
+
+
+}
+
+// classic pass by value usage
+func (p person) updateFirstName1(newFirstName string) {
+
+	p.firstName = newFirstName
+
+}
+
+// pass by reference using the pointer to the struct
+func(personPtr *person) updateFirstName2(newFirstName string) {
+
+	(* personPtr).firstName = newFirstName
+
+}
